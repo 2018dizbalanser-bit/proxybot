7d11ab41e2f1a22b348a4a25f8e18ecc6dcef57c
2018dizbalanser-bit <2018.dizbalanser@gmail.com>
2026-04-13 09:38:12 +0300

    update code

--- keyboards/reply.py
+++ keyboards/reply.py
@@ -13,3 +13,19 @@ def main_keyboard():
 
     builder.adjust(1, 1)  # По одной кнопке в ряд
     return builder.as_markup(resize_keyboard=True)
+
+
+def proxy_actions_kb():
+    builder = ReplyKeyboardBuilder()
+
+    builder.row(
+        types.KeyboardButton(text="🔄 Другой прокси")
+    )
+    builder.row(
+        types.KeyboardButton(text="🔝 Главное меню")
+    )
+
+    return builder.as_markup(
+        resize_keyboard=True,
+        input_field_placeholder="Выберите действие:"
+    )
